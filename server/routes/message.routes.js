b2fc0f474499beb3f2d11f79b3dc4973a69f8b0d
Rocketman <abhinayshambharkar1@gmail.com>
2025-09-04 17:43:49 +0530

    add message model controller apis, create message routes and create routing for messages routes

--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/message.controller";
+import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/message.controller.js";
 import { protectRoute } from "../middlewares/auth.js";
 
 const MessageRouter = express.Router();
