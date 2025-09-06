6729dcd2676a08382eee9cf182684bfe40178ce4
Rocketman <abhinayshambharkar1@gmail.com>
2025-09-06 18:22:52 +0530

    login, signup, logout and updateProfile axios api call funtions

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -29,6 +29,52 @@ export const AuthProvider = ({children}) =>{
             toast.error(err.message);
         }
     }
+
+    // login function to handle user authentication and socket connection.
+    const login = async(state,credentials)=>{
+        // state -> login - login api, and signup state -> signup api
+        try{
+            const data = await axios.post(`/api/auth/${state}`, credentials);
+            if(data.success){
+                setAuthUser(data.userData);
+                connectSocket(data.userData);
+                axios.defaults.headers.common["token"] = data.token;
+                setToken(data.token);
+                localStorage.setItem("token", data.token);
+                toast.success(data.message);
+            }else{
+                toast.error(data.message);
+            }
+        }catch(err){
+            toast.error(err.message);
+        }
+    }
+
+    //logout function to handle user logout and socket disconnection
+    const logout = async()=>{
+        localStorage.removeItem("token");
+        // when we'll remove the token, the user will be logged out.
+        setToken(null);
+        setAuthUser(null);
+        setOnlineUsers([]);
+        axios.defaults.headers.common["token"] = null;
+        toast.success("logged out successfully");
+        socket.disconnect();
+    }
+
+    // update profile function to handle user profile updates
+    const updateProfile = async(body)=>{
+        try{
+            const data = await axios.put("/api/auth/updateProfile",body);
+            if(data.success){
+                setAuthUser(data.user);
+                toast.success("Profile updated successfully");
+            }
+        }catch(err){
+            toast.error(err.message);
+        }
+    }
+
     //but check auth is to be executed whenever website is opened
     useEffect(()=>{
         if(token){
@@ -56,7 +102,10 @@ export const AuthProvider = ({children}) =>{
         axios,
         authUser,
         onlineUsers,
-        socket
+        socket,
+        login,
+        logout,
+        updateProfile
     };
 
     return (
